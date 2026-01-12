ee14a5dce3597db4ca953e58cb85523d0dbf4228
owaiskazmi <owaiskazmi404@gmail.com>
2026-01-12 23:40:33 +0500

    Update table.py

---
+++ Code/table.py
@@ -0,0 +1,9 @@
+num = int (input ("Enter a number: "))
+print ()
+print ("==========================================")
+print ()
+for i in range (1, 13):
+    print ("             ", num, "x", i, "=", num * i)
+print ()
+print ("==========================================")
+print ()
